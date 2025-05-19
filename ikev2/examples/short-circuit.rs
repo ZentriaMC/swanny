9d909aa35ebaed710d4376851f9382b3de880392
Daiki Ueno <ueno@gnu.org>
2025-05-19 14:08:57 +0900

    Split out short-circuit example from tests

---
+++ ikev2/examples/short-circuit.rs
@@ -0,0 +1,112 @@
+use anyhow::Result;
+use bytes::Buf;
+use futures::{
+    SinkExt,
+    channel::mpsc,
+    stream::{FuturesUnordered, StreamExt},
+};
+use std::net::IpAddr;
+use swanny_ikev2::{
+    config::{Config, ConfigBuilder},
+    message::{
+        Message,
+        num::{DhId, EncrId, EsnId, IdType, IntegId, Num, PrfId, Protocol, TrafficSelectorType},
+        payload::Id,
+        serialize::Deserialize,
+        traffic_selector::TrafficSelector,
+    },
+    sa::{ControlMessage, IkeSa},
+};
+
+fn create_config(id: impl AsRef<[u8]>) -> Config {
+    let mut builder = ConfigBuilder::default();
+    builder
+        .ike_proposal(|pc| {
+            pc.encryption(EncrId::ENCR_AES_CBC, Some(256))
+                .prf(PrfId::PRF_HMAC_SHA1)
+                .integrity(IntegId::AUTH_HMAC_SHA1_96)
+                .dh(DhId::SECP256R1)
+                .esn(EsnId::NoEsn)
+                .esn(EsnId::Esn)
+        })
+        .ipsec_protocol(Protocol::ESP)
+        .ipsec_proposal(|pc| {
+            pc.encryption(EncrId::ENCR_AES_CBC, Some(256))
+                .prf(PrfId::PRF_HMAC_SHA1)
+                .integrity(IntegId::AUTH_HMAC_SHA1_96)
+        })
+        .psk(b"test test test")
+        .build(Id::new(Num::Assigned(IdType::ID_KEY_ID), id.as_ref()))
+}
+
+fn create_traffic_selector(address: &IpAddr) -> TrafficSelector {
+    TrafficSelector::new(
+        Num::Assigned(TrafficSelectorType::TS_IPV4_ADDR_RANGE),
+        0,
+        &address,
+        &address,
+        0,
+        0,
+    )
+}
+
+#[tokio::main]
+async fn main() {
+    let config = create_config(b"initiator");
+    let (mut initiator, mut messages_i) = IkeSa::new(&config).expect("unable to create IKE SA");
+
+    let config = create_config(b"responder");
+    let (mut responder, mut messages_r) = IkeSa::new(&config).expect("unable to create IKE SA");
+
+    let mut initiator2 = initiator.clone();
+    let mut responder2 = responder.clone();
+
+    let handle = tokio::spawn(async move {
+        let mut pending_operations = FuturesUnordered::new();
+
+        loop {
+            let res = futures::select! {
+                message = messages_i.select_next_some() => {
+                    match message {
+                        ControlMessage::IkeMessage(message) => {
+                            let serialized_message = message.as_slice();
+                            let mut buf = serialized_message;
+                            let message = Message::deserialize(&mut buf)
+                                .expect("unable to deserialize message");
+                            eprintln!("INITIATOR: {:?}", message);
+                            pending_operations.push(responder2.handle_message(serialized_message.to_vec()));
+                        },
+                        _ => {},
+                    }
+                },
+                message = messages_r.select_next_some() => {
+                    match message {
+                        ControlMessage::IkeMessage(message) => {
+                            let serialized_message = message.as_slice();
+                            let mut buf = serialized_message;
+                            let message = Message::deserialize(&mut buf)
+                                .expect("unable to deserialize message");
+                            eprintln!("RESPONDER: {:?}", message);
+                            pending_operations.push(initiator2.handle_message(serialized_message.to_vec()));
+                        },
+                        _ => {},
+                    }
+                },
+                res = pending_operations.select_next_some() => {
+                    eprintln!("{:?}", res);
+                },
+            };
+        }
+    });
+
+    let initiator_addr: IpAddr = "192.168.1.2".parse().unwrap();
+    let responder_addr: IpAddr = "192.168.1.3".parse().unwrap();
+    let ts_i = create_traffic_selector(&initiator_addr);
+    let ts_r = create_traffic_selector(&responder_addr);
+    initiator
+        .handle_acquire(ts_i, ts_r, 1)
+        .await
+        .expect("unable to handle acquire");
+
+    handle.await.unwrap();
+}
---
+++ ikev2/src/state/established.rs
@@ -0,0 +1,39 @@
+use crate::{
+    config::Config,
+    message::{Message, traffic_selector::TrafficSelector},
+    sa::ControlMessage,
+    state::{State, StateData},
+};
+use anyhow::Result;
+use async_trait::async_trait;
+use futures::channel::mpsc::UnboundedSender;
+use std::sync::Arc;
+use tokio::sync::RwLock;
+
+#[derive(Debug)]
+pub(crate) struct Established {}
+
+#[async_trait]
+impl State for Established {
+    async fn handle_message(
+        self: Box<Self>,
+        _config: &Config,
+        _sender: UnboundedSender<ControlMessage>,
+        _data: Arc<RwLock<StateData>>,
+        _message: &[u8],
+    ) -> Result<Box<dyn State>> {
+        Ok(self)
+    }
+
+    async fn handle_acquire(
+        self: Box<Self>,
+        _config: &Config,
+        _sender: UnboundedSender<ControlMessage>,
+        _data: Arc<RwLock<StateData>>,
+        _ts_i: &TrafficSelector,
+        _ts_r: &TrafficSelector,
+        _index: u32,
+    ) -> Result<Box<dyn State>> {
+        Ok(self)
+    }
+}
