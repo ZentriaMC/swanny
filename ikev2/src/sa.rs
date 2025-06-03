ba4290c03530b8f3b2b1cdda671e3c1f55d65e4a
Daiki Ueno <ueno@gnu.org>
2025-06-04 07:59:07 +0900

    ikev2: Exercise recreation of Child SA

--- ikev2/src/sa.rs
+++ ikev2/src/sa.rs
@@ -922,5 +922,64 @@ mod tests {
         handle.await.expect("handle should be awaited");
 
         assert!(initiator.in_state(&state::Established {}).await);
+
+        // Recreating
+        let initiator2 = initiator.clone();
+
+        let handle = tokio::spawn(async move {
+            let initiator_addr: IpAddr = "192.168.1.2".parse().unwrap();
+            let responder_addr: IpAddr = "192.168.1.3".parse().unwrap();
+            let ts_i = traffic_selector::tests::create_traffic_selector(&initiator_addr);
+            let ts_r = traffic_selector::tests::create_traffic_selector(&responder_addr);
+            initiator2
+                .handle_acquire(ts_i, ts_r, 1)
+                .await
+                .expect("unable to handle acquire");
+        });
+
+        let message = match messages_i.next().await {
+            Some(ControlMessage::IkeMessage(message)) => message,
+            _ => panic!("unexpected message"),
+        };
+
+        handle.await.expect("handle should be awaited");
+
+        assert!(initiator.in_state(&state::NewChildSaRequestSent {}).await);
+
+        let responder2 = responder.clone();
+
+        let handle = tokio::spawn(async move {
+            responder2
+                .handle_message(message)
+                .await
+                .expect("unable to handle message");
+        });
+
+        let message = match messages_r.next().await {
+            Some(ControlMessage::IkeMessage(message)) => message,
+            _ => panic!("unexpected message"),
+        };
+
+        handle.await.expect("handle should be awaited");
+
+        assert!(responder.in_state(&state::Established {}).await);
+
+        let initiator2 = initiator.clone();
+
+        let handle = tokio::spawn(async move {
+            initiator2
+                .handle_message(message)
+                .await
+                .expect("unable to handle message");
+        });
+
+        let _child_sa = match messages_i.next().await {
+            Some(ControlMessage::CreateChildSa(child_sa)) => child_sa,
+            _ => panic!("unexpected message"),
+        };
+
+        handle.await.expect("handle should be awaited");
+
+        assert!(initiator.in_state(&state::Established {}).await);
     }
 }
